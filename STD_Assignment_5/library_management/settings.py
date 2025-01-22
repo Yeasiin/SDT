e93c1b8b79b46ed710b5bf9a0ca7e99c78a95922
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2025-01-22 10:24:18 +0600

    trust

--- STD_Assignment_5/library_management/settings.py
+++ STD_Assignment_5/library_management/settings.py
@@ -33,13 +33,14 @@ SECRET_KEY = 'django-insecure-ti6sd&lv5y(5#(+cz5^5b#64)a-5tk^1$&(n#h766ab6$ton-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*",'https://*.phitron-v1.onrender.com/']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ['https://phitron-v1.onrender.com/']
+CSRF_TRUSTED_ORIGINS = ['https://*.phitron-v1.onrender.com/','https://*.127.0.0.1']
+
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
